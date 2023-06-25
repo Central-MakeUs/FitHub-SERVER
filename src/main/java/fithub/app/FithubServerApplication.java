c8da7027ee57f19d584eb8d5eca20f360839a2ec
CYY1007 <younguk86@gmail.com>
2023-06-25 19:21:58 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/fithub/app/FithubServerApplication.java
@@ -0,0 +1,13 @@
+package fithub.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FithubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FithubServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fithub/app/FithubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fithub.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FithubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
