80be5591c64c419b038a919abf29c9d8a8dd1e5c
CYY1007 <younguk86@gmail.com>
2023-07-10 15:36:43 +0900

    feat : User 회원가입 관련 API 시그니처 작성

--- src/main/java/fithub/app/web/dto/responseDto/ExerciseCategoryResponseDto.java
+++ src/main/java/fithub/app/web/dto/responseDto/ExerciseCategoryResponseDto.java
@@ -2,6 +2,8 @@ package fithub.app.web.dto.responseDto;
 
 import lombok.*;
 
+import java.util.List;
+
 public class ExerciseCategoryResponseDto {
 
     @Builder
@@ -11,5 +13,24 @@ public class ExerciseCategoryResponseDto {
     public static class CategoryDto{
         Integer categoryId;
         String name;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class CategoryFullDto{
+        Integer categoryId;
+        String categoryImage;
+        String name;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class CategoryFullDtoList{
+        List<CategoryFullDto> categoryList;
+        Integer size;
     }
 }
