7e6380ec336e86e6d02009a61c77d4681af2ce72
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-07-16 10:51:44 +0900

    refactor : API 응답 재 수정 (#64)

--- src/main/java/fithub/app/base/ResponseDto.java
+++ src/main/java/fithub/app/base/ResponseDto.java
@@ -9,22 +9,34 @@ import java.util.List;
 @Getter
 @ToString
 @RequiredArgsConstructor
-public class ResponseDto<T> {
+public class ResponseDto<T>{
 
     private final Boolean isSuccess;
     private final Integer code;
     private final String message;
     private final T result;
 
-    public static ResponseDto of(Boolean isSuccess, Code code) {
-        return new ResponseDto(isSuccess, code.getCode(), code.getMessage(), null);
+    public static<T> ResponseDto<T> of(Boolean isSuccess, Code code, T result) {
+        return new ResponseDto<>(isSuccess, code.getCode(), code.getMessage(), result);
     }
 
-    public static ResponseDto of(Boolean isSuccess, Code errorCode, Exception e) {
-        return new ResponseDto(isSuccess, errorCode.getCode(), errorCode.getMessage(e),null);
+    public static <T> ResponseDto<T> of(Code code, T result){
+        return new ResponseDto<>(true, code.getCode(), code.getMessage(), result);
     }
 
-    public static ResponseDto of(Boolean isSuccess, Code errorCode, String message) {
-        return new ResponseDto(isSuccess, errorCode.getCode(), errorCode.getMessage(message),null);
+    public static <T> ResponseDto<T> of(T result){
+        return new ResponseDto<>(true, Code.OK.getCode(), Code.OK.getMessage(), result);
+    }
+
+    public static<T> ResponseDto<T> of(Boolean isSuccess, Code errorCode, Exception e, T result) {
+        return new ResponseDto<>(isSuccess, errorCode.getCode(), errorCode.getMessage(e), result);
+    }
+
+    public static<T> ResponseDto<T> of(Boolean isSuccess, Code errorCode, String message, T result) {
+        return new ResponseDto<>(isSuccess, errorCode.getCode(), errorCode.getMessage(message), result);
+    }
+
+    public static <T> ResponseDto<T> empty(){
+        return new ResponseDto<>(true, Code.OK.getCode(), Code.OK.getMessage(), null);
     }
 }
