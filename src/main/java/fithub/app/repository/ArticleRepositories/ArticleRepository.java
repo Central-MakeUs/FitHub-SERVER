d88d5669896c5a4e3cac9d49a5269b07d69bbaa5
CYY1007 <younguk86@gmail.com>
2023-07-20 21:13:27 +0900

    feat : 게시글 최신순 목록 조회 API 추가

--- src/main/java/fithub/app/repository/ArticleRepositories/ArticleRepository.java
+++ src/main/java/fithub/app/repository/ArticleRepositories/ArticleRepository.java
@@ -1,7 +1,20 @@
 package fithub.app.repository.ArticleRepositories;
 
 import fithub.app.domain.Article;
+import fithub.app.domain.ExerciseCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Page<Article> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    Page<Article> findAllByExerciseCategoryOrderByCreatedAtDesc(ExerciseCategory exerciseCategory, Pageable pageable);
+    Page<Article> findByCreatedAtLessThanOrderByCreatedAtDesc(LocalDateTime createdAt, Pageable pageable);
+
+    Page<Article> findByCreatedAtLessThanAndExerciseCategoryOrderByCreatedAtDesc(LocalDateTime createdAt, ExerciseCategory exerciseCategory, Pageable pageable);
 }
