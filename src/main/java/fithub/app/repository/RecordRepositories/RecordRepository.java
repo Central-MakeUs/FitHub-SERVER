134fb2b626b12831e22e5e42744d56ebb41c84c3
CYY1007 <younguk86@gmail.com>
2023-07-31 03:20:46 +0900

    :recycle: 운동인증 인기순 목록조회 paging 수정

--- src/main/java/fithub/app/repository/RecordRepositories/RecordRepository.java
+++ src/main/java/fithub/app/repository/RecordRepositories/RecordRepository.java
@@ -15,16 +15,10 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 
     Page<Record> findAllByOrderByCreatedAtDesc(Pageable pageable);
     Page<Record> findAllByExerciseCategoryOrderByCreatedAtDesc(ExerciseCategory exerciseCategory, Pageable pageable);
-    Page<Record> findByCreatedAtLessThanOrderByCreatedAtDesc(LocalDateTime createdAt, Pageable pageable);
-    Page<Record> findByCreatedAtLessThanAndExerciseCategoryOrderByCreatedAtDesc(LocalDateTime createdAt, ExerciseCategory exerciseCategory, Pageable pageable);
 
-    Page<Record> findAllByOrderByLikesDesc(Pageable pageable);
+    Page<Record> findByOrderByLikesDescCreatedAtDesc(Pageable pageable);
 
-    Page<Record> findAllByExerciseCategoryOrderByLikesDesc(ExerciseCategory exerciseCategory, Pageable pageable);
-
-    Page<Record> findByLikesLessThanOrderByLikesDesc(Long likes, Pageable pageable);
-
-    Page<Record> findByLikesLessThanAndExerciseCategoryOrderByLikesDesc(Long likes, ExerciseCategory exerciseCategory, Pageable pageable);
+    Page<Record> findByExerciseCategoryOrderByLikesDescCreatedAtDesc(ExerciseCategory exerciseCategory, Pageable pageable);
 
     Page<Record> findByIdInAndCreatedAtLessThanOrderByCreatedAtDesc(List<Long> recordIds, LocalDateTime createdAt, Pageable pageable);
     Page<Record> findByIdInOrderByCreatedAtDesc(List<Long> recordIds,Pageable pageable);
