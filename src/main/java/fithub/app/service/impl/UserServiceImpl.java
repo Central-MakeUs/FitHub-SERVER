7b09157d611a5775d6810b4b3d1ccc22e24841f1
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-08-27 21:36:57 +0900

    Issue/281 ♻️  Refactor : Apple 소셜로그인 수정 (#284)
    * :recycle: Refactor : Apple 소셜로그인 수정
    
    리젝을 피하기 위해 이름을 애플로부터 받아서 저장하도록 수정
    
    * :recycle: Refactor : Apple 소셜로그인 수정
    
    원래 버전 으로 수정

--- src/main/java/fithub/app/service/impl/UserServiceImpl.java
+++ src/main/java/fithub/app/service/impl/UserServiceImpl.java
@@ -463,7 +463,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Transactional(readOnly = false)
-    public OAuthResult.AppleOAuthResultDto appleOAuth(String socialId, String fcmToken, String userName){
+    public OAuthResult.OAuthResultDto appleOAuth(String socialId, String fcmToken, String userName){
         SocialType socialType = SocialType.APPLE;
 
         Boolean isLogin = true;
@@ -479,7 +479,6 @@ public class UserServiceImpl implements UserService {
                             .isSocial(true)
                             .socialId(socialId)
                             .socialType(socialType)
-                            .name(userName)
                             .profileUrl("https://cmc-fithub.s3.ap-northeast-2.amazonaws.com/profile/%EA%B8%B0%EB%B3%B8+%EC%9D%B4%EB%AF%B8%EC%A7%80.png")
                             .communityPermit(Boolean.TRUE)
                             .fcmTokenList(new ArrayList<>())
@@ -508,10 +507,9 @@ public class UserServiceImpl implements UserService {
             accessToken = tokenProvider.createAccessToken(user.getId(), String.valueOf(socialType),socialId, Arrays.asList(new SimpleGrantedAuthority("USER")));
         }
 
-        return OAuthResult.AppleOAuthResultDto.builder()
+        return OAuthResult.OAuthResultDto.builder()
                 .isLogin(isLogin)
                 .accessToken(accessToken)
-                .userName(userName)
                 .userId(user.getId())
                 .build();
     }
