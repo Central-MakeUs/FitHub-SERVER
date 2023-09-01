219cf29695a25abfdbb8ab6673ce070d507b457c
CYY1007 <younguk86@gmail.com>
2023-09-01 23:48:15 +0900

    :bug: Fix : swagger cors 에러 수정

--- src/main/java/fithub/app/config/WebConfig.java
+++ src/main/java/fithub/app/config/WebConfig.java
@@ -30,6 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
         CorsConfiguration configuration = new CorsConfiguration();
         // 모든 출처를 허용합니다.
         configuration.setAllowedOrigins(Arrays.asList("https://www.profithub.store"));
+        configuration.setAllowedOrigins(Arrays.asList("https://api.profithub.store"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
